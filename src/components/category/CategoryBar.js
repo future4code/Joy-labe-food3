9048b5c4fb80a4a31aedfd27172d7a01cc6af9f1
Karla Natany <karlanatany3@gmail.com>
2022-02-23 19:20:34 -0300

    filtros

--- src/components/category/CategoryBar.js
+++ src/components/category/CategoryBar.js
@@ -1,11 +1,20 @@
+import { AppBar, Tab, Tabs } from '@material-ui/core'
 import React from 'react'
 import { ContainerCategory, ItemCategory } from './styles'
 
-export const CategoryBar = (props) => {
-  return (
+export const CategoryBar = ({button, filter}) => {
+  return(
     <div>
-      <ItemCategory> {props.category}</ItemCategory>
-    </div>
-
-  )
+        {button?.map((category) => {
+          return(
+          <button type="button" onClick={() => filter(category)}>
+            {category}
+          </button>)
+        })}
+    </div>)
 }
+
+
+{/* <ItemCategory 
+        onClick={props.onClick} 
+        value={props.value}> {props.category}</ItemCategory> */}
--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -4,73 +4,89 @@ import { CardComponent } from "./CardComponent";
 import GlobalStateContext from "../../global/GlobalStateContext";
 import Header from "../../components/header/Header";
 import { Container } from "../restaurantsPage/styles";
-import {Footer} from "../../components/footer/Footer"
+import { Footer } from "../../components/footer/Footer"
 import { Input, TextField } from "@material-ui/core";
 import { CategoryBar } from "../../components/category/CategoryBar";
 import { ContainerCategory } from "../../components/category/styles";
+import { useRequestData } from "../../hooks/useRequestData";
 
 export default function HomePage(props) {
-    const { states} = useContext(GlobalStateContext);
+    const { states } = useContext(GlobalStateContext);
     const { restaurantes } = states;
     const [search, setSearch] = useState("")
+    const [buttons, setButtons] = useState('carregando')
 
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
-    }
-    
+
+    //renderização da lista completa na tela
     const listRestaurant = restaurantes && restaurantes.map((restaurant) => {
         return (
             <CardComponent
-            key={restaurant.id}
-            image={restaurant.products[4].photoUrl}
-            placeName={restaurant.name}
-            deliveryTime={restaurant.deliveryTime}
-            deliveyValue={restaurant.shipping}
-            pathName={restaurant.id}
-            />
-            )
-        })
-
-    const filteredRestaurant = useMemo(() => {
-        if(!search) return listRestaurant;
-
-        return listRestaurant.filter( restaurantItem => {
-            return restaurantItem.props.placeName.toLowerCase().includes( search.toLowerCase() )
-        })
-    }, [search, listRestaurant])
-
-    const listCategory = restaurantes && restaurantes.map((restaurant) => {
-        return (
-            <CategoryBar
-            key={restaurant.id}
-            category={restaurant.category}
+                key={restaurant.id}
+                image={restaurant.products[4].photoUrl}
+                placeName={restaurant.name}
+                deliveryTime={restaurant.deliveryTime}
+                deliveyValue={restaurant.shipping}
+                pathName={restaurant.id}
             />
         )
     })
-    // .filter((produto) => { 
 
-    //     return props.classificacaoCategoria === "Todas as categorias" || produto.categoria.includes(props.classificacaoCategoria) // }) 
-    // <Box component="div" sx={{ overflow: 'hidden' }}> Estouro oculto </Box> <Box component="div" sx={{ overflow: 'visible' }}> Estouro visível </Box> >
+    //FILTRO PELO NOME DO RESTAURANTE
+    //input campo de pesquisa texto
+    const handleSearch = (e) => setSearch(e.target.value)
 
-    return (
-      <MainContainer>
+    //função de filtro pegando valor do input de texto
+    const filteredRestaurant = useMemo(() => {
+        if (!search) return listRestaurant;
+        return listRestaurant.filter(restaurantItem => {
+            return restaurantItem.props.placeName.toLowerCase().includes(search.toLowerCase())
+        })
+    }, [search, listRestaurant])
+
+    //FILTRO DE CATEGORIA
+
+    const allCategories = ['Todas', ...(restaurantes && restaurantes.map(item => item.category))]
+    console.log(allCategories);
+
+    const filter = (button) => {
+        if (button === 'Todas') return listRestaurant
+        return listRestaurant.filter(item => item.category === button)
+    }
+
+
+//map para mostrar as categorias dos restaurantes
+// const listCategory = restaurantes && restaurantes.map((restaurant) => {
+//     return (
+//         <CategoryBar
+//             key={restaurant.id}
+//             category={restaurant.category}
+//         />
+//     )
+// })
+
+// const handleCategory = () => {
+
+// }
+
+
+return (
+    <MainContainer>
         <Header
-        pageName={"SnackTime"}
-    />
-            <hr/>
-           
-           <Container>
-          <form>
-          <TextField value={search} onChange={handleSearch} label="Restaurante" variant="outlined" fullWidth/>
-          </form>
-          <ContainerCategory>
-          {listCategory}
-          </ContainerCategory>
-          
+            pageName={"SnackTime"}
+        />
+        <hr />
+
+        <Container>
+            <form>
+                <TextField value={search} onChange={handleSearch} label="Restaurante" variant="outlined" fullWidth />
+            </form>
+            <ContainerCategory>
+                <CategoryBar button={buttons} filter={filter}/>
+            </ContainerCategory>
 
             {filteredRestaurant}
-            </Container>
-            <Footer/>
-        </MainContainer>
-    )
-};
+        </Container>
+        <Footer />
+    </MainContainer>
+)
+}
